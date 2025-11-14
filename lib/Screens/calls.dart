fb93e42e7109a3f7cd2a88eb159023509b60e884
shawonworks <ahshawon777@gmail.com>
2025-11-14 17:03:04 +0600

    camera commited

---
+++ lib/Screens/calls.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class Calls extends StatefulWidget {
+  const Calls({super.key});
+
+  @override
+  State<Calls> createState() => _CallsState();
+}
+
+class _CallsState extends State<Calls> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-
-
-
-
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -63,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Icon(Icons.search, color: Colors.white),
             SizedBox(width: 10),
             PopupMenuButton(
-              icon: Icon(Icons.more_vert,color: Colors.white,),
+              icon: Icon(Icons.more_vert, color: Colors.white),
               itemBuilder: (context) => [
                 PopupMenuItem(value: 1, child: Text("New Group")),
                 PopupMenuItem(value: 2, child: Text("New Broadcast")),
@@ -76,38 +69,46 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: TabBarView(
           children: [
-            Text("Camera"),
+            ColoredBox(color: Colors.black),
             ListView.builder(
               itemCount: 50,
-              itemBuilder: (context, index){
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage("https://media.istockphoto.com/id/1399565382/photo/young-happy-mixed-race-businessman-standing-with-his-arms-crossed-working-alone-in-an-office.jpg?s=612x612&w=is&k=20&c=SRtmXotZfERsp4_0tTn24AXdCTWyssWO8r6dj5ehI98=")
-
-
-                ),
-                title: Text("Arfan Hasnat Shawon"),
-                subtitle: Text("How are you"),
-                trailing: Text("12:40 AM"),
-                textColor: Colors.black,
-              );
-            }),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      "https://media.istockphoto.com/id/1399565382/photo/young-happy-mixed-race-businessman-standing-with-his-arms-crossed-working-alone-in-an-office.jpg?s=612x612&w=is&k=20&c=SRtmXotZfERsp4_0tTn24AXdCTWyssWO8r6dj5ehI98=",
+                    ),
+                  ),
+                  title: Text("Erfan Hasnat Shawon"),
+                  subtitle: Text("How are you"),
+                  trailing: Text("12:40 AM"),
+                  textColor: Colors.black,
+                );
+              },
+            ),
             Text("Status"),
             ListView.builder(
               itemCount: 50,
-              itemBuilder: (context, index){
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage("https://media.istockphoto.com/id/1399565382/photo/young-happy-mixed-race-businessman-standing-with-his-arms-crossed-working-alone-in-an-office.jpg?s=612x612&w=is&k=20&c=SRtmXotZfERsp4_0tTn24AXdCTWyssWO8r6dj5ehI98=")
-
-
-                ),
-                title: Text("Arfan Hasnat Shawon"),
-                subtitle: Text(index /5 ==0?"you missed audio call" : "you missed video call"),
-                trailing: Icon(index /5 ==0? Icons.phone : Icons.video_call),
-                textColor: Colors.black,
-              );
-            }),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      "https://media.istockphoto.com/id/1399565382/photo/young-happy-mixed-race-businessman-standing-with-his-arms-crossed-working-alone-in-an-office.jpg?s=612x612&w=is&k=20&c=SRtmXotZfERsp4_0tTn24AXdCTWyssWO8r6dj5ehI98=",
+                    ),
+                  ),
+                  title: Text("Arfan Hasnat Shawon"),
+                  subtitle: Text(
+                    index / 5 == 0
+                        ? "you missed audio call"
+                        : "you missed video call",
+                  ),
+                  trailing: Icon(
+                    index / 5 == 0 ? Icons.phone : Icons.video_call,
+                  ),
+                  textColor: Colors.black,
+                );
+              },
+            ),
             Text("Call"),
           ],
         ),
